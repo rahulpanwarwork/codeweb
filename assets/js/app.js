72a1375ae0bb35f2464ab53cac5dd19093d44152
RahulPanwar <rahulpanwarwork@gmail.com>
2021-04-26 23:40:42 +0530

    Header Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,10 @@
 function darken_screen(yesno) {
   if (yesno == true) {
     document.querySelector(".screen-darken").classList.add("active");
+    document.querySelector("html").style.overflowY = "hidden";
   } else if (yesno == false) {
     document.querySelector(".screen-darken").classList.remove("active");
+    document.querySelector("html").style.overflowY = "auto";
   }
 }
 
@@ -11,14 +13,12 @@ function close_offcanvas() {
   darken_screen(false);
   document.querySelector(".mobile-offcanvas.show").classList.remove("show");
   document.body.classList.remove("offcanvas-active");
-  // document.querySelector(".mobile-toggle").classList.add("bg-white");
 }
 
 function show_offcanvas(offcanvas_id) {
   darken_screen(true);
   document.getElementById(offcanvas_id).classList.add("show");
   document.body.classList.add("offcanvas-active");
-  // document.querySelector(".mobile-toggle").classList.remove("bg-white");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -50,7 +50,7 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("subHeader").style.transform = "translateY(-45px)";
+    document.getElementById("subHeader").style.transform = "translateY(-48px)";
     document.getElementById("navbarMain").classList.add("navbar-sticky-dark");
     document.getElementById("mobileToggle").classList.add("mobile-toggle-dark");
   } else {
