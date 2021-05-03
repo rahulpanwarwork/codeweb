b97d7e0c46d2a29a391ad8e38f35974f28c7eec5
RahulPanwar <rahulpanwarwork@gmail.com>
2021-05-03 21:02:59 +0530

    Contact page header replace

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,12 +66,12 @@ function scrollFunction() {
 }
 
 // // Page Loader Start
-// document.onreadystatechange = function () {
-//   if (document.readyState !== "complete") {
-//     document.querySelector("body").style.visibility = "hidden";
-//     document.querySelector(".loader").style.visibility = "visible";
-//   } else {
-//     document.querySelector(".loader").style.display = "none";
-//     document.querySelector("body").style.visibility = "visible";
-//   }
-// };
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
